b0c46abea1dcb7b31c23b0b4efbbf8348d189f2b
Sebastien Leboucher <sebastien.leboucher@gmail.com>
2020-03-31 14:19:59 +0200

    hibernate tests

--- src/main/java/com/example/project/dao/JpaBookmarkRepository.java
+++ src/main/java/com/example/project/dao/JpaBookmarkRepository.java
@@ -33,19 +33,23 @@ public interface JpaBookmarkRepository extends JpaRepository<Bookmark, String> {
     @Query("select distinct bk from Bookmark bk")
     List<Bookmark> findAllWithGraphAttr();
 
+    // pas toute la grappe ici pour répondre au pb du n+1 hibernate avec un requete supplémentaire en mode BatchSize
     @EntityGraph(attributePaths = {"owner", "supports"})
     @Query("select distinct bk from Bookmark bk")
     Page<Bookmark> findAllPagination(Pageable pageable);
 
+    // pas toute la grappe ici pour répondre au pb du n+1 hibernate avec un requete supplémentaire en mode BatchSize
     @EntityGraph(attributePaths = {"owner", "supports"})
     @Query("select distinct bk from Bookmark bk")
     Slice<Bookmark> findAllSlice(Pageable pageable);
 
+    // projection classique avec HQL
     @Query("select new com.example.project.dao.projection.BookmarkView(bk.id, bk.url, bk.name, bk.owner.name) " +
             "from Bookmark bk " +
             "LEFT JOIN bk.owner ")
     List<BookmarkView> findWithOwner();
 
+    // projection avec native query et mapping via interface/proxy
     @Query(nativeQuery = true,
             value = "select bm.id as id, bm.url as url, bm.name as bookmarkName, ow.name as ownerName " +
                     "from BOOKMARK bm left outer join OWNER ow on bm.owner_id=ow.id")
