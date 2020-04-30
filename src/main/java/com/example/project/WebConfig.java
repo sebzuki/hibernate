23a4cd4a26f071ab1480f262d45292f8c84c588b
Sebastien Leboucher <sebastien.leboucher@gmail.com>
2020-04-30 20:48:32 +0200

    update

---
+++ src/main/java/com/example/project/WebConfig.java
@@ -0,0 +1,19 @@
+/*
+ * Sébastien Leboucher
+ */
+package com.example.project;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
